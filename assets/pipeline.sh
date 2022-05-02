fa0796846eddd75464dfad1bb91aad5a072b15b3
missyprs <missyprs@gmail.com>
2022-05-02 12:36:50 +1200

    Added in the pipeline script it currently only has the install, run and build npm commands

---
+++ assets/pipeline.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "running npm commands"
+#run the npm install
+npm install
+#then run the npm build to make sure that it compiles
+npm run build
+#then run the start
+#npm run start
