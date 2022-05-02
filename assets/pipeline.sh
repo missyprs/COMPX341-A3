d1a96daf046ce42560bdc7d4857b5d4b38f1aa38
missyprs <missyprs@gmail.com>
2022-05-02 15:23:37 +1200

    script

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+if [[ -z $1 ]]
+then
+  echo "please input a commit message"
+  exit 2
+fi
 
 echo "running npm commands"
 #run the npm install
 npm install
 #then run the npm build to make sure that it compiles
 npm run build
-#then run the start
-#npm run start
 
 cd ../
 git add .
@@ -15,3 +18,5 @@ git push origin main
 git status
 
 echo $1 "done"
+#then run the start
+#npm run start
