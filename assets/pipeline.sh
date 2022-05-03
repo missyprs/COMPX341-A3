4e148abe86daa84d1d75a09e2e90df55f8130f0d
missyprs <missyprs@gmail.com>
2022-05-04 09:34:53 +1200

    exit code test if this commits there was no exit code

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -4,7 +4,7 @@ then
   echo "please input a commit message"
   exit 2
 fi
-
+#set the S variable to the commit message
 S="$*"
 
 echo "running npm commands"
@@ -12,13 +12,24 @@ echo "running npm commands"
 npm install
 #then run the npm build to make sure that it compiles
 npm run build
+E="$?"
 
-cd ../
-git add .
-git commit -m "$S"
-git push origin main
-git status
+#if the run build exits with a code other than 0
+echo $E #this tells me what code the build exited with.  if it is more than 0 there was a compile error
+
+if [[ "$E" -eq "0" ]]
+then
+  echo "no compile error"
+  cd ../
+  git add .
+  git commit -m "$S"
+  git push origin main
+  git status
+else
+  echo "compile error"
+  exit 1
+fi
 
 echo $S
 #then run the start
-#npm run start
+npm run start
