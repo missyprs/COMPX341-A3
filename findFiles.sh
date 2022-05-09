6214bcc1430a3504ac1be5b08bbc49b35a291c75
missyprs <missyprs@gmail.com>
2022-05-10 10:25:58 +1200

    COMPX341 Pipeline Maintained

--- findFiles.sh
+++ findFiles.sh
@@ -2,6 +2,7 @@
 
 #this finds all of the files with a .ts extension and runs it through the java file
 
-echo "finding things"
+echo "Finding all .ts extension files to change"
+echo "Please wait for this script to finish"
 
 find . -type f -name '*.ts' -exec java AddLine {} \;
