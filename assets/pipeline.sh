c73e91896eddfce0249ea3fcadfaa91eb3a837f5
missyprs <missyprs@gmail.com>
2022-05-10 10:23:05 +1200

    testing change to pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+
+echo "(0) Preparing Pipeline"
 #checks if there is a commit message
 if [[ -z $1 ]]
 then
-  echo "Please input a commit message"
+  echo "Error: No Commit Message"
   exit 1
 fi
 
-echo "this bash file assumes that the commit message is enclosed in double quotes \" \" if not please reset the commit message in double quotes"
-
+echo "(1) Build (Compiling Application)"
 npm install #run the npm install
 npm run build #then run the npm build to make sure that it compiles
 E="$?" #E holds the exit code of npm run build
@@ -15,17 +16,21 @@ E="$?" #E holds the exit code of npm run build
 #if run build exits with exit code 0
 if [[ "$E" -eq "0" ]]
 then #commit to git
-  cd ../
+  echo "Build Succeeded"
+else
+  #the exit code was not 0 so there was an error
+  echo "Error: Build Failed"
+  exit 1
+fi
+
+echo "(2) Release to Git"
+cd ../
   git add .
   git commit -m "$1"
   git push origin main
   git status
-else
-  #the exit code was not 0 so there was an error
-  echo "There was a compile error and no commit occured"
-  exit 1
-fi
 
+echo "(3) Deploying Application"
 #move back into the assets folder to be able to run the site
 cd assets
 #then run the start
